4dad07410f0f4346adc09273794cd5c4e98789ce
jmorris <jim.morris@mobilepointofsales.com>
2019-08-05 07:51:13 -0700

    develop

--- app/src/main/java/com/greendot/rewards/Constants.kt
+++ app/src/main/java/com/greendot/rewards/Constants.kt
@@ -3,18 +3,18 @@ package com.greendot.rewards
 import com.greendot.rewards.repository.Movie
 
 object Constants {
-    // Network
-    val MOVIES_ARRAY_DATA_TAG = "results"
+
+    const val MOVIES_ARRAY_DATA_TAG = "results"
     val MOVIE_ARRAY_LIST_CLASS_TYPE = (ArrayList<Movie>()).javaClass
-    val POPULAR_MOVIES_BASE_URL = "https://api.themoviedb.org/3/discover/"
-    private val IMAGE_URL_PREFIX = "https://image.tmdb.org/t/p/"
-    val SMALL_IMAGE_URL_PREFIX = IMAGE_URL_PREFIX + "w300"
-    val BIG_IMAGE_URL_PREFIX = IMAGE_URL_PREFIX + "w500"
+    const val POPULAR_MOVIES_BASE_URL = "https://api.themoviedb.org/3/discover/"
+    private const val IMAGE_URL_PREFIX = "https://image.tmdb.org/t/p/"
+    const val SMALL_IMAGE_URL_PREFIX = IMAGE_URL_PREFIX + "w300"
+    const val BIG_IMAGE_URL_PREFIX = IMAGE_URL_PREFIX + "w500"
     const val API_KEY_REQUEST_PARAM = "api_key"
     const val LANGUAGE_REQUEST_PARAM = "language"
     const val PAGE_REQUEST_PARAM = "page"
-    val API_KEY = "17c5889b399d3c051099e4098ad83493"
-    val LANGUAGE = "en"
-    val LOADING_PAGE_SIZE = 20
-    // DB
+    const val API_KEY = "17c5889b399d3c051099e4098ad83493"
+    const val LANGUAGE = "en"
+    const val LOADING_PAGE_SIZE = 20
+
 }
